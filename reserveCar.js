8f7f33726fd9249ae7fef2313a9f0c7e0dc6f6a1
vuanhngo <vuanh1403@gmail.com>
2023-10-22 11:31:17 +0800

    current working filters

--- reserveCar.js
+++ reserveCar.js
@@ -16,8 +16,17 @@ function loadCarData() {
 // Function to render car cards based on data
 function renderCars(cars) {
     const carList = document.getElementById('car');
+    const makeFilter = document.getElementById('filter-make').value;
+    const priceFilter = parseInt(document.getElementById('filter-price').value);
+
+    carList.innerHTML = '';
+
 
     cars.forEach((car) => {
+        if (
+            (makeFilter === 'all' || car.make.toLowerCase() === makeFilter) &&
+            (priceFilter === 0 || car.price <= priceFilter)
+        ) {
         const carCard = document.createElement('div');
         carCard.classList.add('car');
         carCard.innerHTML = `
@@ -27,8 +36,18 @@ function renderCars(cars) {
             <button>Book Now</button>
         `;
         carList.appendChild(carCard);
+        }
     });
 }
 
 
 window.onload = loadCarData;
+
+// Reload cars when change filters
+document.getElementById('filter-make').addEventListener('change', () => {
+    renderCars(cars);
+});
+
+document.getElementById('filter-price').addEventListener('change', () => {
+    renderCars(cars);
+});
