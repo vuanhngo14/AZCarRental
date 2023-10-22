e5cdf47fc6fcea0ede5a472a61599bd06200ded8
vuanhngo <vuanh1403@gmail.com>
2023-10-22 11:21:27 +0800

    load cars from json file with no filter

--- reserveCar.js
+++ reserveCar.js
@@ -1,53 +1,34 @@
-// Sample car data
-const cars = [
-    {
-        id: 1,
-        make: 'toyota',
-        model: 'Roadster',
-        price: 300,
-        image: 'img/roadster.jpeg',
-    },
-    {
-        id: 2,
-        make: 'Tesla',
-        model: 'Model X',
-        price: 250,
-        image: 'img/modelX.webp',
-    },
-    // Add more car data as needed
-];
+let cars; 
 
-// Function to render car cards based on filters
-function renderCars() {
-    const makeFilter = document.getElementById('filter-make').value;
-    const priceFilter = document.getElementById('filter-price').value;
-    const carList = document.getElementById('car-list');
+// Function to load car data from JSON and display it in the HTML
+function loadCarData() {
+    fetch('cars.json')
+        .then((response) => response.json())
+        .then((data) => {
+            cars = data; 
+            renderCars(cars);
+        })
+        .catch((error) => {
+            console.error('Error loading car data: ' + error);
+        });
+}
 
-    // Clear the existing car listings
-    carList.innerHTML = '';
+// Function to render car cards based on data
+function renderCars(cars) {
+    const carList = document.getElementById('car');
 
     cars.forEach((car) => {
-        // Apply filters
-        if (
-            (makeFilter === 'all' || car.make === makeFilter) &&
-            (priceFilter === 'all' || car.price <= parseInt(priceFilter))
-        ) {
-            const carCard = document.createElement('div');
-            carCard.classList.add('car');
-            carCard.innerHTML = `
-                <img src="${car.image}" alt="${car.model}">
-                <h3>${car.make} ${car.model}</h3>
-                <p>Price: $${car.price}/day</p>
-                <button>Book Now</button>
-            `;
-            carList.appendChild(carCard);
-        }
+        const carCard = document.createElement('div');
+        carCard.classList.add('car');
+        carCard.innerHTML = `
+            <img src="${car.image}" alt="${car.model}">
+            <h3>${car.make} ${car.model}</h3>
+            <p>Price: $${car.price}/day</p>
+            <button>Book Now</button>
+        `;
+        carList.appendChild(carCard);
     });
 }
 
-// Add event listeners for filter options
-document.getElementById('filter-make').addEventListener('change', renderCars);
-document.getElementById('filter-price').addEventListener('change', renderCars);
 
-// Initial rendering
-renderCars();
+window.onload = loadCarData;
