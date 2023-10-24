78fa7bbce4cd0f5761bbeea0eb912541e2e5c666
Vu Anh Ngo <vuanh1403@gmail.com>
2023-10-24 23:34:03 +0800

    save to local storage

--- reserveCarPayment.js
+++ reserveCarPayment.js
@@ -37,5 +37,43 @@ function displayCarDetails() {
     }
 }
 
+// Function to handle form submission
+function handleSubmit(event) {
+    event.preventDefault();
+
+    // Get car ID 
+    const urlParams = new URLSearchParams(window.location.search);
+    const carId = urlParams.get('id');
+  
+    const form = document.getElementById('rental-form');
+  
+    // Get the values from the form fields
+    const fullName = form.querySelector('#full-name').value;
+    const contactNumber = form.querySelector('#contact-number').value;
+    const email = form.querySelector('#email').value;
+    const currentDate = new Date();
+  
+    // Create a rental object
+    const rentalInfo = {
+      carId,
+      fullName,
+      contactNumber,
+      email,
+      rentalDateTime: currentDate.toISOString(),
+    };
+  
+    // Store the rental data in local storage
+    const rentalData = JSON.parse(localStorage.getItem('rentalData')) || [];
+    rentalData.push(rentalInfo);
+    localStorage.setItem('rentalData', JSON.stringify(rentalData));
+  
+    alert('Thank you for your reservation!');
+      form.reset();
+  }
+  
+  document.getElementById('rental-form').addEventListener('submit', handleSubmit);
+  
+
+
 // Run the function when the page loads
 window.onload = loadCarData;
