56b25321d27c034fdb48719c16294cb87917be1e
Vu Anh Ngo <vuanh1403@gmail.com>
2023-10-25 11:52:59 +0800

    can search and display cars

--- returnCar.js
+++ returnCar.js
@@ -25,7 +25,6 @@ function collectNames() {
     const rentalData = JSON.parse(localStorage.getItem('rentalData')) || [];
 
     const matchingCarIds = rentalData.filter((entry) => entry.fullName === fullName && entry.contactNumber === contactNumber).map((entry) => entry.carId);
-    console.log(matchingCarIds.length); 
 
     displayCarData(matchingCarIds);
 };
@@ -37,19 +36,71 @@ function displayCarData(matchingCarIds) {
     } else {
         console.log("running display car function");
         matchingCarIds.forEach((carId) => {
-            const matchingCar = cars.find((car) => car.id === carId);
+
+            var carIdInt = parseInt(carId, 10);
+
+            const matchingCar = cars.find((car) => car.id === carIdInt);
 
             if (matchingCar) {
-                // Create and append elements to display car data
-                const rentedCarDiv = document.createElement('div');
-                rentedCarDiv.classList.add('rented-car');
-
-                const showDetailsDiv = document.createElement('div');
-                showDetailsDiv.classList.add('show-details');
-                showDetailsDiv.innerHTML = `<p>Car ID: ${matchingCar.id}</p><p>Name: ${matchingCar.make} ${matchingCar.model}</p><p>Total Price: $${matchingCar.price}</p>`;
-
-                rentedCarDiv.appendChild(showDetailsDiv);
-                carDetailsDiv.appendChild(rentedCarDiv);
+                if (matchingCar) {
+                    // Create and append elements to display car data
+                    const rentedCarDiv = document.createElement('div');
+                    rentedCarDiv.classList.add('rented-car');
+                
+                    // Create showDetailsDiv and its children
+                    const showDetailsDiv = document.createElement('div');
+                    showDetailsDiv.classList.add('show-details');
+                
+                    // Create elements for car data
+                    const carIdLabelSpan = document.createElement('span');
+                    carIdLabelSpan.textContent = "Car ID: ";
+                
+                    const carIdSpan = document.createElement('span');
+                    carIdSpan.id = 'car-id';
+                    carIdSpan.textContent = matchingCar.id;
+                
+                    const carNameLabelSpan = document.createElement('span');
+                    carNameLabelSpan.textContent = "Name: ";
+                
+                    const carNameSpan = document.createElement('span');
+                    carNameSpan.id = 'car-name';
+                    carNameSpan.textContent = `${matchingCar.make} ${matchingCar.model}`;
+                
+                    const carPriceLabelSpan = document.createElement('span');
+                    carPriceLabelSpan.textContent = "Total Price: ";
+                
+                    const carPriceSpan = document.createElement('span');
+                    carPriceSpan.id = 'car-price';
+                    carPriceSpan.textContent = `$${matchingCar.price}`;
+                
+                    // Append the elements to their respective parents
+                    showDetailsDiv.appendChild(carIdLabelSpan);
+                    showDetailsDiv.appendChild(carIdSpan);
+                    showDetailsDiv.appendChild(document.createElement('br'));
+                    showDetailsDiv.appendChild(carNameLabelSpan);
+                    showDetailsDiv.appendChild(carNameSpan);
+                    showDetailsDiv.appendChild(document.createElement('br'));
+                    showDetailsDiv.appendChild(carPriceLabelSpan);
+                    showDetailsDiv.appendChild(carPriceSpan);
+                
+                    // Create showButtonsDiv and its children
+                    const showButtonsDiv = document.createElement('div');
+                    showButtonsDiv.classList.add('show-buttons');
+                
+                    const returnCarButton = document.createElement('button');
+                    returnCarButton.id = 'return-car';
+                    returnCarButton.textContent = 'Return car';
+                
+                    // Append the elements to rentedCarDiv
+                    rentedCarDiv.appendChild(showDetailsDiv);
+                    rentedCarDiv.appendChild(showButtonsDiv);
+                    showButtonsDiv.appendChild(returnCarButton);
+                
+                    // Append rentedCarDiv to the list-rented-car div
+                    const listRentedCarDiv = document.querySelector('.list-rented-car');
+                    listRentedCarDiv.appendChild(rentedCarDiv);
+                }
+                
             }
         });
     }
